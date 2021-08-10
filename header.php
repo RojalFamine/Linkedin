7f92ca326ee9c4a007c4ee9816d57089c26b7604
RoyalFamine <rojal7famine@gmail.com>
2021-08-10 11:24:56 +0300

    added bootstrap and moved header to header.php

---
+++ header.php
@@ -0,0 +1,64 @@
+<div class="header-back">
+      <div class="header">
+        <div class="left-side">
+          <img src="inlogo.svg" alt="logo" class="logo" />
+          <form action="">
+            <div class="search-box">
+              <input type="text" placeholder="Search" />
+              <svg
+                width="9"
+                height="9"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M10.1779 2.8233C12.2088 4.85421 12.2088 8.14696 10.1779 10.1779C8.14696 12.2088 4.85421 12.2088 2.8233 10.1779C0.79239 8.14696 0.79239 4.85421 2.8233 2.8233C4.85421 0.79239 8.14696 0.79239 10.1779 2.8233ZM11.6589 10.4578C13.6195 7.90868 13.4323 4.23907 11.0972 1.90398C8.55856 -0.634659 4.44261 -0.634659 1.90398 1.90398C-0.634659 4.44261 -0.634659 8.55856 1.90398 11.0972C4.23907 13.4323 7.90867 13.6195 10.4578 11.6589L14.1894 15.667C14.5928 16.1002 15.2752 16.1124 15.6938 15.6938C16.1124 15.2752 16.1002 14.5928 15.6669 14.1894L11.6589 10.4578Z"
+                  fill="#66676B"
+                />
+              </svg>
+            </div>
+          </form>
+        </div>
+        <div class="right-side">
+          <nav class="header-nav">
+            <a href="#">
+              <i class="icon-home"></i>
+              <div>Home</div>
+            </a>
+            <a href="#">
+              <i class="icon-friends"></i>
+              <span class="notification">5</span>
+              <div>My Network</div>
+            </a>
+            <a href="#">
+              <i class="icon-job"></i>
+              <div>Jobs</div>
+            </a>
+            <a href="#">
+              <i class="icon-chat"></i>
+              <span class="notification">19</span>
+              <div>Messaging</div>
+            </a>
+            <a href="#">
+              <i class="icon-notification"></i>
+              <span class="notification">5</span>
+              <div>Notifications</div>
+            </a>
+          </nav>
+          <div class="profiles">
+            <a href="#" class="my-prof">
+              <div class="prof-pic"></div>
+              <div class="prof-txt">Me <i class="icon-arrow"></i></div>
+            </a>
+            <div class="border"></div>
+            <a href="#" class="work-prof">
+              <i class="icon-menu"></i>
+              <div class="prof-txt">Work <i class="icon-arrow"></i></div>
+            </a>
+          </div>
+          <a href="#" class="free">Try Premium Free<br />for 1 Month</a>
+        </div>
+      </div>
+    </div>
